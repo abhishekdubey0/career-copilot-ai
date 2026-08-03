956935cbfc3afd0fc1c8a6a2bede0c1a8263f494
abhishekdubey <ad47288@gmail.com>
2026-08-03 10:04:46 +0530

    feat(auth): initialize auth service

---
+++ infrastructure/postgres/init/init.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE auth_db;
+
+CREATE DATABASE resume_db;
+
+CREATE DATABASE job_db;
+
+CREATE DATABASE interview_db;
+
+CREATE DATABASE analytics_db;
+
+CREATE DATABASE notification_db;
